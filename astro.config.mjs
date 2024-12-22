a3f76594f0752193e52af85bf5f504c03b70da9f
Jeremy Reduxion <jeremy@reduxion.com.my>
2024-12-23 05:40:05 +0800

    vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import relativeLinks from "astro-relative-links";
 
 import preact from "@astrojs/preact";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://reduxion.com.my/pro/astrotutorial/",
-    integrations: [relativeLinks(), preact()]
+  site: "https://reduxion.com.my/pro/astrotutorial/",
+  integrations: [relativeLinks(), preact()],
+  adapter: vercel()
 });
