07458fc7226dc10504275c3a488e5f20abbf9216
Jeremy Reduxion <jeremy@reduxion.com.my>
2024-12-23 05:31:10 +0800

    tutorial completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import relativeLinks from "astro-relative-links";
+
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://reduxion.com.my/pro/astrotutorial/",
+    integrations: [relativeLinks(), preact()]
+});
